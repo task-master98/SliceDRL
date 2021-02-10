fd3d5719026739c133a0982a56799de4a31b7859
LAPTOP-P7CUSO1H\ishaa <ishaan.roy@sleepiz.com>
2021-02-10 11:20:08 +0530

    Commented class KDTree since its temporarily unused
    - changed kdt arguments from lists to numpy arrays

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import math
-
+import numpy as np
 from sklearn.neighbors import KDTree as kdt
 
 def distance(a, b):
@@ -8,39 +8,39 @@ def distance(a, b):
 # Initial connections using k-d tree
 def kdtree(clients, base_stations):
 
-    c_coor = [(c.x,c.y) for c in clients]
-    bs_coor = [p.coverage.center for p in base_stations]
+    c_coor = np.array([(c.x,c.y) for c in clients])
+    bs_coor = np.array([p.coverage.center for p in base_stations])
 
-    tree = KDTree(bs_coor, leaf_size=2)
-    res = tree.query(c_coor)
+    tree = kdt(bs_coor, leaf_size=2)
+    res = tree.query(c_coor, k=min(5, len(base_stations)))
 
     for c, d, p in zip(clients, res[0], res[1]):
         if d[0] <= base_stations[p[0]].coverage.radius:
             c.base_station = base_stations[p[0]]
 
-class KDTree:
-    last_run_time = 0
-    limit = None
+# class KDTree:
+#     last_run_time = 0
+#     limit = 5
 
-    # Initial connections using k-d tree
-    @staticmethod
-    def run(clients, base_stations, run_at, assign=True):
-        print(f'KDTREE CALL [{run_at}] - limit: {KDTree.limit}')
-        if run_at == KDTree.last_run_time:
-            return
-        KDTree.last_run_time = run_at
+#     # Initial connections using k-d tree
+#     @staticmethod
+#     def run(clients, base_stations, run_at, assign=True):
+#         print(f'KDTREE CALL [{run_at}] - limit: {KDTree.limit}')
+#         if run_at == KDTree.last_run_time:
+#             return
+#         KDTree.last_run_time = run_at
         
-        c_coor = [(c.x,c.y) for c in clients]
-        bs_coor = [p.coverage.center for p in base_stations]
+#         c_coor = [(c.x,c.y) for c in clients]
+#         bs_coor = [p.coverage.center for p in base_stations]
 
-        tree = kdt(bs_coor, leaf_size=2)
-        res = tree.query(c_coor,k=min(KDTree.limit,len(base_stations)))
+#         tree = kdt(bs_coor, leaf_size=2)
+#         res = tree.query(c_coor,k=min(KDTree.limit,len(base_stations)))
 
-        # print(res[0])
-        for c, d, p in zip(clients, res[0], res[1]):
-            if assign and d[0] <= base_stations[p[0]].coverage.radius:
-                c.base_station = base_stations[p[0]]    
-            c.closest_base_stations = [(a, base_stations[b]) for a,b in zip(d,p)]
+#         # print(res[0])
+#         for c, d, p in zip(clients, res[0], res[1]):
+#             if assign and d[0] <= base_stations[p[0]].coverage.radius:
+#                 c.base_station = base_stations[p[0]]    
+#             c.closest_base_stations = [(a, base_stations[b]) for a,b in zip(d,p)]
 
 
 def format_bps(size, pos=None, return_float=False):
